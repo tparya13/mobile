94d3abd4317218f51f5860156dcf8ec25819a1fb
arya <aryatheruparambil@gmail.com>
2024-01-31 13:22:31 +0530

    file added

--- Todoproject/Todoproject/urls.py
+++ Todoproject/Todoproject/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('TodoApp.urls')), 
+    path('auth/',include('userarauth.urls')), 
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
